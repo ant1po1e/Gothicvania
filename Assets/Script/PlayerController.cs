4468b9b431ed4a5909e082ec2e48d5844a2516cd
Yudha Pratama <symamaxim@gmail.com>
2023-04-10 14:59:48 +0700

    first commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed, jumpHeight;
+    float velX, velY;
+    Rigidbody2D rb;
+    public Transform groundcheck;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundcheck.position, groundCheckRadius, whatIsGround);
+        if (isGrounded)
+        {
+            anim.SetBool("Jump", false);
+        }
+        else
+        {
+            anim.SetBool("Jump", true);
+        }
+
+
+        FlipCharacter();
+        Attack();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        Jump();
+    }
+
+    public void Attack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetBool("Attack", true);
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+        }
+    }
+
+    void Movement()
+    {
+        velX = Input.GetAxisRaw("Horizontal");
+        velY = rb.velocity.y;
+        rb.velocity = new Vector2(velX * speed, velY);
+        if (rb.velocity.x != 0)
+        {
+            anim.SetBool("Walk", true);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    }
+
+    void FlipCharacter()
+    {
+        if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(3, 3, 3);
+        }
+        else if(rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-3, 3, 3);
+        }
+    }
+}
